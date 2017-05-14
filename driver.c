023c6cb472857a6d9020035ea08bdf65d860005e
SheepAvenger <robert.mccook.elm@gmail.com>
2017-05-13 22:32:23 -0700

    Add files via upload

--- driver.c
+++ driver.c
@@ -1,36 +1,56 @@
-{Program Sample
-	{Function facto VAL
-		{if {< VAL 0 }
-		then {= retVal -1}
-		else {= retVal 1}
-		}
-		{while {> VAL 0} do
-			{= retVal {* retVal VAL}}
-			{= VAL {- VAL 1}}
-		}
-	return retVal
-	}
-	{Function rtrn a b
-  		{if {< a b}
-   		then {= ret -0.001}
-		else {= ret 0.001}
-		}
-   		{if {> a b}
-   		then { = ret 0.001}
-   		else {= ret 0}
-  		}
-	return ret
-	}
+//#include "lex.yy.c"
+#include<stdlib.h>
+#include<stdio.h>
+#include "FP.tab.h"
+#include "scanner.h"
+#define DEBUG 0
 
-	{Function getEle index lst
-      		{if {!= index lst}
-       		then {(pound f)}
-		else {(pound t)}
-      		}
-      		{if {== index 1}
-        	 then {= lst lst-1}
-		 else {= lst lst}
-		}
-	return lst
-	}
+int isPrime(int p);
+int myPrime;
+
+void main()
+{
+    //extern FILE *yyin;
+    FILE *yyin = stdin;
+    long int size;
+    fseek(yyin, 1, SEEK_END);
+	printf("point 1\n");
+    size = ftell(yyin);
+    rewind(yyin);
+    if(size > 271)
+    {
+        size /= 16;
+        myPrime = size -1;
+        for(; ; myPrime--)
+        {
+            if(isPrime(myPrime))
+            break;
+        }
+    }
+    else
+        myPrime = 13; // default prime for file size smaller than 272
+    if(DEBUG) {printf("Prime: %d\n\n", myPrime);}    
+    createTable(myPrime);
+    
+    do {
+        int t = yyparse();
+	printf("t %i\n", t);
+    } while(!feof(yyin));
+	fclose(yyin);
+   printf("\n========= Finished reading the input file =========\n");
+	printTable();
+	  
+    printf("\n");   
+	printBlocks(); 
+    
+}
+
+int isPrime(int p)
+{
+    int i;
+    for(i = 2; p%i != 0; i++);
+    if(p==i)
+        return 1;
+    else
+        return 0;
 }
